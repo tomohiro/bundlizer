4b9d35ca4dca4a8dbe51d4b5b48d1d18b6c5a881
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2012-01-27 11:45:37 +0900

    fix echo command

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 set -e
 
+
 echo 'Start install the Bundlizer...'
-  git clone git://github.com/Tomohiro/bundlizer.git $HOME/.bundlizer
+git clone git://github.com/Tomohiro/bundlizer.git $HOME/.bundlizer
 
 echo
-echo 'Finished. Please type this command.'
-echo '  `echo 'source $HOME/.bundlizer/etc/bashrc' >> ~/.bash_profile`'
+echo 'Finished. Please run this command.'
+echo
+echo "  \`echo 'source \"\$HOME/.bundlizer/etc/bashrc\"' >> ~/.bash_profile\`"

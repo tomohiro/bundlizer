52531a9da0ca0634ee0c83da6b3b5e5333b79662
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-01-24 18:14:32 +0900

    fix test

--- test/run_all_tests.sh
+++ test/run_all_tests.sh
@@ -6,8 +6,8 @@
 # Test runner
 
     run_all_tests() {
-      for test in $(ls); do
-        sh $test
+      for test in $(ls test); do
+        sh test/$test
         [ $? != 0 ] && {
           echo "Oops, test failed."
           exit 1

dc9bfe131469fec5b5c61e257c2aacc0d155650e
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-02-05 18:22:16 +0900

    add depends test

--- test/gem_install_test.sh
+++ test/gem_install_test.sh
@@ -5,3 +5,6 @@
 test_do "bundlizer install pry -v=0.9.9"
 test_do "bundlizer update pry"
 test_do "bundlizer which pry"
+test_do "bundlizer install backup"
+test_do "bundlizer depends backup --add-gem net-ssh -v '~> 2.3.0'"
+test_do "bundlizer depends backup --add-gem net-scp -v '~> 1.0.4'"

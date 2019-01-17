ddf5a6122a435f9b683f83792904da148a56c6aa
Tomohiro Taira <tomohiro.t@gmail.com>
2019-01-17 20:14:09 +0900

    Support XDG_DATA_HOME environment variable

--- install.sh
+++ install.sh
@@ -2,11 +2,15 @@
 
 set -e
 
+BUNDLIZER_ROOT="${HOME}/.bundlizer"
+if [ $XDG_DATA_HOME ]; then
+  BUNDLIZER_ROOT="${XDG_DATA_HOME}/bundlizer"
+fi
 
 echo 'Start install the Bundlizer...'
-git clone git://github.com/Tomohiro/bundlizer.git $HOME/.bundlizer
+  git clone git://github.com/tomohiro/bundlizer.git "${BUNDLIZER_ROOT}"
 
 echo
 echo 'Finished. Please run this command.'
 echo
-echo "  \`echo 'source \"\$HOME/.bundlizer/etc/bashrc\"' >> ~/.bash_profile\`"
+echo "  \`echo 'source \"${BUNDLIZER_ROOT}/etc/bashrc\"' >> ~/.bash_profile\`"
